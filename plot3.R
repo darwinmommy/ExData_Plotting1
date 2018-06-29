7f69f2f587118572db97f92325a100a8c936c7c2
darwinmommy <darwinmommy@gmail.com>
2018-06-28 17:08:37 -0700

    Update plot3.R
    Changed code from dev.copy to begin writing file at start, legend did not transfer appropriately

--- plot3.R
+++ plot3.R
@@ -29,6 +29,10 @@ mydata2$Sub_metering_3<-as.numeric(mydata2$Sub_metering_3)
 
 
 #Plot 3:
+#Begin writing png file
+png("plot3.png")
+#Set parameters, single graph in window
+par(mfrow=c(1,1))
 #Create line graphs for 3 separate datasets: Sub metering 1, 2, and 3, start with a blank x-axis
 plot(mydata2$Sub_metering_1, type="o",
      pch=".", ylab="Energy sub metering", xlab="",xaxt="n")
@@ -36,11 +40,10 @@ points(mydata2$Sub_metering_2,type="o",
      pch=".", col="red")
 points(mydata2$Sub_metering_3,type="o",
      pch=".",col="blue")
-#Reformat ticks and labels on xaxis
+#Reformat ticks and labels on x-axis
 axis(1,at=c(1,1440,2880),labels = c("Thu","Fri","Sat"))
 #Add a legend
 legend("topright",legend = c("Sub_metering_1","Sub_metering_2","Sub_metering_3"),
        col=c("black","red","blue"),lty = 1)
-#Save the plot as a png file
-dev.copy(png,"plot3.png")
+#Finish writing png file
 dev.off()
